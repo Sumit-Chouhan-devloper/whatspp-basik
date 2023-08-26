10c0a461239493739bbc108a95cc26a24a90c1f4
Naveen <naveenninu888@gmail.com>
2023-08-26 11:55:23 +0530

    css set

--- pages/components/popups/QrCodeScan.jsx
+++ pages/components/popups/QrCodeScan.jsx
@@ -11,7 +11,7 @@ function QrCodeScan() {
         <>
             <div className="qr_bg_fade">
                 <div className="bg_light_green pb-4 ff_inter">
-                    <div className="bg_light_green container qr_container pt-4 pt-md-5 pb-5 ">
+                    <div className="bg_light_green container qr_container pt-4 pt-md-5 pb-5 px-0 ">
                         <div className="d-flex align-items-center mb-4 mb-md-5 pb-lg-5">
                             <Link href="/" className='text-decoration-none'>
                                 <Image src={Logo} alt="Logo" className='qr_logo' />
@@ -22,24 +22,27 @@ function QrCodeScan() {
                     </div>
                 </div>
 
-                <div className="qr_container container bg-white qr_scaner_box ff_inter pt-5">
+                <div className="qr_container container bg-white qr_scaner_box ff_inter pt-5 px-0">
                     <div className="row px-md-3 pt-2xl-5 px-xl-5 mx-2xl-5">
-                        <div className="col-lg-7 m-auto">
+                        <div className="col-lg-7 m-auto m-lg-0">
                             <h1 className='font-2xl fw_light color_dark_black mb-xl-5 pb-4' >Use whatsApp on your Computer</h1>
                             <p className='font-lg fw-normal color_dark_black mt-2 mb-0' >1. Open whatsApp on your phone</p>
                             <p className='font-lg fw-normal color_dark_black py-4 mb-0' >2. Tap Menu <Image src={Menu} alt="menu" /> or Setting <Image src={Setting} alt="setting" /> and select Linked Decives</p>
                             <p className='font-lg fw-normal color_dark_black mb-0' >3. Tap on Link a Device</p>
                             <p className='font-lg fw-normal color_dark_black mb-0 pt-4' >4. Point your phone to this screen to capture the code</p>
                         </div>
-                        <div className="col-lg-5 mt-md-5 mt-lg-0">
-                            <Image src={Scan} alt="Logo" className='w-100 px-4' />
+                        <div className="col-lg-5 mt-md-5 mt-lg-0 text-center">
+                            <Image src={Scan}
+                                alt="Scan"
+                                width={377}
+                                height={379} />
                         </div>
                         <span className='d-block border-bottom pb-md-5 pt-2xl-4 mt-xxl-2 mb-4 mb-md-5'></span>
                     </div>
                     <p className=' px-md-3 text-center text-lg-start ps-2xl-5 ms-2xl-5 fs_mmd color_lite_green fw-normal mb-2xl-5 pb-3'>Link with phone number</p>
                     <div className="text-center qr_bg_tutorial pt-2xl-5">
                         <h1 className='font-2xl fw_light color_dark_black mb-0 pt-4' >Tutorial</h1>
-                        <p className=' ps-xxl-5 ms-xxl-5 fs_mmd color_lite_green fw-normal py-3 mb-0'>Need help to get started?</p>
+                        <p className=' px-xxl-5 mx-xxl-5 fs_mmd color_lite_green fw-normal py-3 mb-0'>Need help to get started?</p>
                     </div>
                 </div >
 
