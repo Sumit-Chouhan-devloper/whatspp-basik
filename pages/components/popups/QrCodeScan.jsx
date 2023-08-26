566e23f074d66701b3ffeaa342e0fbfca605f6c4
Naveen <naveenninu888@gmail.com>
2023-08-26 14:34:24 +0530

    spacing issue solve

--- pages/components/popups/QrCodeScan.jsx
+++ pages/components/popups/QrCodeScan.jsx
@@ -12,7 +12,7 @@ function QrCodeScan() {
             <div className="qr_bg_fade">
                 <div className="bg_light_green pb-4 ff_inter">
                     <div className="bg_light_green container qr_container pt-4 pt-md-5 pb-5 px-0 ">
-                        <div className="px-3 mx-md-0 d-flex align-items-center mb-4 mb-md-5 pb-lg-5">
+                        <div className="px-3 mx-1 mx-md-0 d-flex align-items-center mb-4 mb-md-5 pb-lg-5">
                             <Link href="/" className='text-decoration-none'>
                                 <Image src={Logo} alt="Logo" className='qr_logo' />
                             </Link>
@@ -23,8 +23,8 @@ function QrCodeScan() {
                 </div>
 
                 <div className="qr_container container bg-white qr_scaner_box ff_inter pt-5 px-0">
-                    <div className="row px-3 justify-content-between px-lg-5  mx_scaner_xxl_space ">
-                        <div className="col-lg-6 col-xl-7 px-3 px-lg-0 m-auto mx-lg-0">
+                    <div className="row mx-2 justify-content-between px-lg-5  mx_scaner_xxl_space ">
+                        <div className="col-lg-6 col-xl-7 px-lg-0 m-auto mx-lg-0">
                             <h1 className='font-2xl fw_light color_dark_black mb-xl-5 pb-4 mb-0 mb-md-2' >Use whatsApp on your Computer</h1>
                             <p className='font-lg fw-normal color_dark_black mt-md-2 mb-0' >1. Open whatsApp on your phone</p>
                             <p className='font-lg fw-normal color_dark_black py-2 py-md-4 mb-0' >2. Tap Menu <Image src={Menu} alt="menu" className='menu_size_scan' /> or Setting <Image src={Setting} alt="setting" className='menu_size_scan' /> and select Linked Decives</p>
