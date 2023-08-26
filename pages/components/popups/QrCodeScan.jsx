bfb2b4a837912c482f0dc16d478aa9c0af55f217
Naveen <naveenninu888@gmail.com>
2023-08-26 14:20:32 +0530

    code scan complete

--- pages/components/popups/QrCodeScan.jsx
+++ pages/components/popups/QrCodeScan.jsx
@@ -23,7 +23,7 @@ function QrCodeScan() {
                 </div>
 
                 <div className="qr_container container bg-white qr_scaner_box ff_inter pt-5 px-0">
-                    <div className="row px-3 justify-content-between px-lg-5 mx_scaner_xxl_space ">
+                    <div className="row px-3 justify-content-between px-lg-5  mx_scaner_xxl_space ">
                         <div className="col-lg-6 col-xl-7 px-3 px-lg-0 m-auto mx-lg-0">
                             <h1 className='font-2xl fw_light color_dark_black mb-xl-5 pb-4' >Use whatsApp on your Computer</h1>
                             <p className='font-lg fw-normal color_dark_black mt-2 mb-0' >1. Open whatsApp on your phone</p>
@@ -40,7 +40,7 @@ function QrCodeScan() {
                         </div>
                         <span className='d-block border-bottom pb-md-5 pt-2xl-4 mt-2xl-2 mb-4 mb-md-5 mt-2xl-2'></span>
                     </div>
-                    <p className=' px-md-3 text-center text-lg-start ps-2xl-5 ms-2xl-5 fs_mmd color_lite_green fw-normal mb-2xl-5 pb-3'>Link with phone number</p>
+                    <p className=' px-md-3 text-center text-lg-start fs_mmd color_lite_green fw-normal mb-2xl-5 pb-3'>Link with phone number</p>
                     <div className="text-center qr_bg_tutorial pt-2xl-5">
                         <h1 className='font-2xl fw_light color_dark_black mb-0 pt-4' >Tutorial</h1>
                         <p className=' px-xxl-5 mx-xxl-5 fs_mmd color_lite_green fw-normal py-3 mb-0'>Need help to get started?</p>
