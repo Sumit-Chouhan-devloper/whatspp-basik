c76dba8d7dde0f039b851b8bcfb8399a5722750d
Neha <neharadialcode@gmail.com>
2023-10-26 12:31:47 +0530

    fav icon added

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
-import React from 'react'
-import Whatsapp from '../components/homepage/Whatsapp'
- 
+import React from "react";
+import Whatsapp from "../components/homepage/Whatsapp";
+import Head from "next/head";
+
 export default function Home() {
   return (
     <>
-<Whatsapp/>
+      <Head>
+        <link
+          rel="icon"
+          href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxcGVijgvw_eyTh2UDl2Fbfv2oVD9ffgi2AOo9ZcP7Yff7FHjAHMIs3gTkjSM0249lQ0E&usqp=CAU"
+        />
+      </Head>
+      <Whatsapp />
     </>
-  )
+  );
 }
